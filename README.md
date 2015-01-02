d27230e770aef739b5833cd75b4f2d9ae6281453
st98 <st98js@gmail.com>
2015-01-03 08:02:08 +0900

    アセンブリ修行

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-asm-practice
-============
-
+# asm-practice
 アセンブリ修行。練習で書いたブートローダなんかを投げます。
+
+## ファイルの説明
+- [happy-new-year.s](happy-new-year.s)
+ - 初めて書いたブートローダ。
+ - `"Happy New Year!\r\n(^_^)\r\n"` を出力するだけ。
---
+++ happy-new-year.s
@@ -0,0 +1,32 @@
+bits 16
+org 0x7c00
+
+boot:
+  xor ax, ax
+  mov ds, ax
+
+  cld
+  mov si, msg
+
+loop:
+  lodsb
+  test al, al
+  jz end
+
+  mov ah, 0x0e
+  mov bh, 0x00
+  mov bl, 0x07
+  int 0x10
+
+  jmp loop
+
+end:
+  hlt
+
+msg:
+  db "Happy New Year!", 0x0d, 0x0a
+  db "(^_^)", 0x0d, 0x0a, 0x00
+
+times 0x200 - 2 - ($ - $$) db 0
+db 0x55
+db 0xaa
